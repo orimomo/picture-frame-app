0f8a987f2fb79246aef8503c7a426d0a4fe136d7
momo <mmko5110@gmail.com>
2019-10-30 00:25:07 +0900

    ボタンを押せるようにするところまで

--- app/src/main/java/com/example/picture_frame_app/MainActivity.kt
+++ app/src/main/java/com/example/picture_frame_app/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.picture_frame_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            // 画像選択する処理
+        }
     }
 }
