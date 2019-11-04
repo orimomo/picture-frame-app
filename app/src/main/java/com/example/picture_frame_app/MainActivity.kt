9ccda10aa8742106f45d9094fbf3dea28b32c5c4
momo <mmko5110@gmail.com>
2019-11-04 17:17:58 +0900

    ギャラリーから選択した画像をImageViewに表示させる

--- app/src/main/java/com/example/picture_frame_app/MainActivity.kt
+++ app/src/main/java/com/example/picture_frame_app/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.picture_frame_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import android.graphics.BitmapFactory
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +16,40 @@ class MainActivity : AppCompatActivity() {
 
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener {
-            // 画像選択する処理
+            selectPicture()
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != RESULT_OK) {
+            return
+        }
+        when (requestCode) {
+            READ_REQUEST_CODE -> {
+                try {
+                    data?.data?.also { uri ->
+                        val inputStream = contentResolver?.openInputStream(uri)
+                        val image = BitmapFactory.decodeStream(inputStream)
+                        val imageView = findViewById<ImageView>(R.id.imageView)
+                        imageView.setImageBitmap(image)
+                    }
+                } catch (e: Exception) {
+                    Toast.makeText(this, "エラーが発生しました", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    private fun selectPicture() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "image/*"
+        }
+        startActivityForResult(intent, READ_REQUEST_CODE)
+    }
+
+    companion object {
+        private const val READ_REQUEST_CODE: Int = 42
     }
 }
